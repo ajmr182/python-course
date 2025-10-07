799f52744ca81436b632b22168d97cd645e195e7
Luis Beltran <luisbeltran501@gmail.com>
2025-10-07 01:12:02 -0300

    KAN 6 Beltran

---
+++ hola/SemaforoBeltran.py
@@ -0,0 +1,10 @@
+color = input("Ingresa un color del semáforo: ").lower()
+
+if color == "rojo":
+    print("No Cruces, espera")
+elif color == "amarillo":
+    print("Preparate, aún no cruces")
+elif color == "verde":
+    print("Cruza verdecon cuidado")
+else:
+    print("Ingresa un color válido")
